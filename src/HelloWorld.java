3459f68e94bbae8c3d8efae83e3402e49d17402b
Kylie <miss.kyliegilbert@gmail.com>
2025-05-24 20:55:29 -0400

    Added Source and HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("I'm tired. Really.")
+        System.out.println("Goodbye world!");
+        System.out.println("Off to the Digital Ether I go");
+    }
+}
